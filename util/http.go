3cb4e3d1dfa31e45608210d43de26f82537b154d
Alberto García Hierro <alberto@garciahierro.com>
2012-11-04 03:31:15 +0100

    Add From() function
    Returns the same-site url the user came from

--- util/http.go
+++ util/http.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -21,4 +22,20 @@ func RemoteAddress(r *http.Request) string {
 		remote = strings.Split(remote, ":")[0]
 	}
 	return remote
+}
+
+func From(r *http.Request) string {
+	from := r.FormValue("from")
+	if from == "" {
+		ref := r.Referer()
+		if ref != "" {
+			refURL, err := url.Parse(ref)
+			if err == nil {
+				if r.Host == refURL.Host {
+					from = ref
+				}
+			}
+		}
+	}
+	return from
 }

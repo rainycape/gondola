f41cf4af52d8f099265a540bb6feca1b0818c4b0
Alberto García Hierro <alberto@garciahierro.com>
2013-03-19 00:47:49 +0100

    Add serialize pkg
    Adds function for easier serialization of objects to json or xml

---
+++ serialize/serialize.go
@@ -0,0 +1,43 @@
+package serialize
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"net/http"
+	"strconv"
+)
+
+type Serializer int
+
+const (
+	Json Serializer = iota
+	Xml
+)
+
+func Write(w http.ResponseWriter, value interface{}, s Serializer) (int, error) {
+	var contentType string
+	var data []byte
+	var err error
+	if s == Json {
+		data, err = json.Marshal(value)
+		contentType = "application/json"
+	} else if s == Xml {
+		data, err = xml.Marshal(value)
+		contentType = "application/xml"
+	}
+	if err != nil {
+		return 0, err
+	}
+	total := len(data)
+	header := w.Header()
+	header.Set("Content-Type", contentType)
+	header.Set("Content-Length", strconv.Itoa(total))
+	for c := 0; c < total; {
+		n, err := w.Write(data)
+		c += n
+		if err != nil {
+			return n, err
+		}
+	}
+	return n, nil
+}

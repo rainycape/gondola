713ee061e0cbb80eb017beb1fb210595bfc9c1af
Alberto García Hierro <alberto@garciahierro.com>
2013-03-21 18:19:42 +0100

    Document pkg serialize

--- serialize/serialize.go
+++ serialize/serialize.go
@@ -1,3 +1,5 @@
+// Package serialize provides conveniency functions
+// for serializing values to either JSON or XML
 package serialize
 
 import (
@@ -15,6 +17,10 @@ const (
 	Xml
 )
 
+// Write serializes value using the SerializationFormat f and writes it
+// to w. If w also implements http.ResponseWriter, it sets the appropiate
+// headers too. Returns the number of bytes written and any error that might
+// occur while serializing or writing the serialized data.
 func Write(w io.Writer, value interface{}, f SerializationFormat) (int, error) {
 	var contentType string
 	var data []byte
@@ -48,10 +54,12 @@ func Write(w io.Writer, value interface{}, f SerializationFormat) (int, error) {
 	return total, nil
 }
 
+// WriteJson is equivalent to Write(w, value, Json)
 func WriteJson(w io.Writer, value interface{}) (int, error) {
 	return Write(w, value, Json)
 }
 
+// WriteXml is equivalent to Write(w, value, Xml)
 func WriteXml(w io.Writer, value interface{}) (int, error) {
 	return Write(w, value, Xml)
 }

6aaf7b88de8e7e55c3bf0eb8a736167d8c2bc80f
Alberto García Hierro <alberto@garciahierro.com>
2013-04-23 22:45:28 +0200

    Add support for automatic compression

--- cache/cache.go
+++ cache/cache.go
@@ -1,8 +1,12 @@
 package cache
 
 import (
+	"bytes"
+	"compress/zlib"
 	"gondola/log"
+	"io"
 	"net/url"
+	"strconv"
 )
 
 type BackendInitializer func(*url.URL) Backend
@@ -27,9 +31,11 @@ type Codec struct {
 }
 
 type Cache struct {
-	Prefix  string
-	Backend Backend
-	Codec   *Codec
+	Prefix            string
+	Backend           Backend
+	Codec             *Codec
+	MinCompressLength int
+	CompressLevel     int
 }
 
 func (c *Cache) manipulatesKeys() bool {
@@ -118,7 +124,24 @@ func (c *Cache) GetMulti(keys []string) map[string]interface{} {
 // the given key. See the documentation for Set for an
 // explanation of the timeout parameter
 func (c *Cache) SetBytes(key string, b []byte, timeout int) {
-	log.Debugf("Setting key %s", c.backendKey(key))
+	if c.MinCompressLength >= 0 {
+		if l := len(b); l > c.MinCompressLength {
+			var buf bytes.Buffer
+			w, err := zlib.NewWriterLevel(&buf, c.CompressLevel)
+			if err == nil {
+				w.Write(b)
+				w.Close()
+				cb := buf.Bytes()
+				if cl := len(cb); cl < l {
+					b = cb
+					log.Debugf("Compressed key %s from %d bytes to %d bytes", key, l, cl)
+				}
+			} else {
+				log.Warningf("Error opening zlib writer: %s", err)
+			}
+		}
+	}
+	log.Debugf("Setting key %s (%d bytes)", c.backendKey(key), len(b))
 	err := c.Backend.Set(c.backendKey(key), b, timeout)
 	if err != nil {
 		log.Errorf("Error setting cache key %s: %s", key, err)
@@ -131,6 +154,21 @@ func (c *Cache) GetBytes(key string) []byte {
 	if err != nil {
 		log.Errorf("Error getting cache key %s: %s", key, err)
 		return nil
+	}
+	if c.MinCompressLength >= 0 && len(b) > 0 {
+		log.Debugf("Decompressing key %s", key)
+		r, err := zlib.NewReader(bytes.NewBuffer(b))
+		if err == nil {
+			var buf bytes.Buffer
+			_, err = io.Copy(&buf, r)
+			if err == nil {
+				if r.Close() == nil {
+					b = buf.Bytes()
+				}
+			} else {
+				r.Close()
+			}
+		}
 	}
 	return b
 }
@@ -163,7 +201,10 @@ func DefaultUrl() string {
 }
 
 func New(cacheUrl string) *Cache {
-	cache := &Cache{}
+	cache := &Cache{
+		MinCompressLength: -1,
+		CompressLevel:     zlib.DefaultCompression,
+	}
 	var query url.Values
 	u, err := url.Parse(cacheUrl)
 	if err != nil {
@@ -184,6 +225,27 @@ func New(cacheUrl string) *Cache {
 			}
 		}
 		cache.Prefix = query.Get("prefix")
+		if mcl := query.Get("min_compress"); mcl != "" {
+			val, err := strconv.Atoi(mcl)
+			if err == nil {
+				cache.MinCompressLength = val
+			} else {
+				log.Warningf("Invalid min_compress value %q (must be integer)", mcl)
+			}
+		}
+		if cl := query.Get("compress_level"); cl != "" {
+			val, err := strconv.Atoi(cl)
+			if err == nil {
+				if val >= zlib.NoCompression && val <= zlib.BestCompression {
+					cache.CompressLevel = val
+				} else {
+					log.Warningf("Invalid compress_level value %d (must be between %d and %d)",
+						val, zlib.NoCompression, zlib.BestCompression)
+				}
+			} else {
+				log.Warningf("Invalid compress_level value %q (must be integer)", cl)
+			}
+		}
 	}
 	if codec == nil {
 		codec = &GobEncoder

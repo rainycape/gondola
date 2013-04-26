91627f8270b3f6f6a524a3e5b5406ca65da68b55
Alberto García Hierro <alberto@garciahierro.com>
2013-04-26 14:02:35 +0200

    Enable Lshortlevel by default

--- log/log.go
+++ log/log.go
@@ -13,15 +13,15 @@ const (
 	// order they appear (the order listed here) or the format they present (as
 	// described in the comments).  A colon appears after these items:
 	//	2009/0123 01:23:23.123123 /a/b/c/d.go:23: message
-	Ldate         = 1 << iota                // the date: 2009/01/23
-	Ltime                                    // the time: 01:23:23
-	Lmicroseconds                            // microsecond resolution: 01:23:23.123123.  assumes Ltime.
-	Llongfile                                // full file name and line number: /a/b/c/d.go:23
-	Lshortfile                               // final file name element and line number: d.go:23. overrides Llongfile
-	Llevel                                   // preprend the message level
-	Lshortlevel                              // preprend the abbreviated message level (overrides Llevel)
-	Lcolored                                 // uses colors around the level name
-	LstdFlags     = Ldate | Ltime | Lcolored // initial values for the standard logger
+	Ldate         = 1 << iota                              // the date: 2009/01/23
+	Ltime                                                  // the time: 01:23:23
+	Lmicroseconds                                          // microsecond resolution: 01:23:23.123123.  assumes Ltime.
+	Llongfile                                              // full file name and line number: /a/b/c/d.go:23
+	Lshortfile                                             // final file name element and line number: d.go:23. overrides Llongfile
+	Llevel                                                 // preprend the message level
+	Lshortlevel                                            // preprend the abbreviated message level (overrides Llevel)
+	Lcolored                                               // uses colors around the level name
+	LstdFlags     = Ldate | Ltime | Lshortlevel | Lcolored // initial values for the standard logger
 )
 
 var (

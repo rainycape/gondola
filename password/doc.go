a2584e37b6cf8de3e9640df40e068cf244b050ab
Alberto García Hierro <alberto@garciahierro.com>
2013-07-11 00:19:16 +0200

    Documentation update

--- password/doc.go
+++ password/doc.go
@@ -6,4 +6,27 @@
 // Password provides the Check() method for verifying that
 // the given plaintext matches the encoded password. This
 // method is not vulnerable to timing attacks.
+//
+// Password objects can be stored directly by Gondola's ORM.
+//
+//  // "foo" is the username, "bar" is the password.
+//  type User struct {
+//	UserId int64 `orm:",primary_key,auto_increment"`
+//	Username string
+//	Password password.Password
+//  }
+//  // Creating a new user
+//  user := &User{Username:"foo", Password: password.New("bar")}
+//  // o is a gondola/orm.Orm object
+//  o.MustSave(user)
+//  // Signin in an existing user
+//  var user *User
+//  if err := o.One(orm.Eq("Username", "foo"), &user); err == nil {
+//	if user.Password.Check("bar") == nil {
+//	    // user has provided the correct password
+//	}
+//  }
+//
+// Password objects can also be stored on anything that accepts strings. See
+// the examples to learn how to manually store and verify a password.
 package password

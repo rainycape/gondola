e6205b82086c1d3d3dc2c4f1c835858ea4f700af
Alberto García Hierro <alberto@garciahierro.com>
2013-07-10 20:57:17 +0200

    Use gondola/types.IsTrue rather than the local func isTrue

--- template/funcs.go
+++ template/funcs.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"gondola/assets"
+	"gondola/types"
 	"html/template"
 	"reflect"
 	"strconv"
@@ -129,7 +130,7 @@ func concat(args ...interface{}) string {
 func and(args ...interface{}) bool {
 	for _, v := range args {
 		val := reflect.ValueOf(v)
-		t, _ := isTrue(val)
+		t, _ := types.IsTrue(val)
 		if !t {
 			return false
 		}
@@ -140,7 +141,7 @@ func and(args ...interface{}) bool {
 func or(args ...interface{}) bool {
 	for _, v := range args {
 		val := reflect.ValueOf(v)
-		t, _ := isTrue(val)
+		t, _ := types.IsTrue(val)
 		if t {
 			return true
 		}
@@ -150,43 +151,12 @@ func or(args ...interface{}) bool {
 
 func not(arg interface{}) bool {
 	val := reflect.ValueOf(arg)
-	t, _ := isTrue(val)
+	t, _ := types.IsTrue(val)
 	return !t
 }
 
 func now() time.Time {
 	return time.Now()
-}
-
-// isTrue returns whether the value is 'true', in the sense of not the zero of its type,
-// and whether the value has a meaningful truth value. This function is a copy of the
-// one found in text/template
-func isTrue(val reflect.Value) (truth, ok bool) {
-	if !val.IsValid() {
-		// Something like var x interface{}, never set. It's a form of nil.
-		return false, true
-	}
-	switch val.Kind() {
-	case reflect.Array, reflect.Map, reflect.Slice, reflect.String:
-		truth = val.Len() > 0
-	case reflect.Bool:
-		truth = val.Bool()
-	case reflect.Complex64, reflect.Complex128:
-		truth = val.Complex() != 0
-	case reflect.Chan, reflect.Func, reflect.Ptr, reflect.Interface:
-		truth = !val.IsNil()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		truth = val.Int() != 0
-	case reflect.Float32, reflect.Float64:
-		truth = val.Float() != 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		truth = val.Uint() != 0
-	case reflect.Struct:
-		truth = true // Struct values are always true.
-	default:
-		return
-	}
-	return truth, true
 }
 
 var templateFuncs template.FuncMap = template.FuncMap{

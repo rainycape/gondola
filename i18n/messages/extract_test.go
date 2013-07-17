4d6e48325c7972d3c84c7d17e6e9e8254ef6d069
Alberto García Hierro <alberto@garciahierro.com>
2013-07-17 15:34:18 +0200

    test_data -> _test_data
    The latter is ignored by godoc

--- i18n/messages/extract_test.go
+++ i18n/messages/extract_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestExtract(t *testing.T) {
 	log.SetLevel(log.LDebug)
-	m, err := Extract("test_data", DefaultFunctions(), DefaultTypes(), DefaultTagFields())
+	m, err := Extract("_test_data", DefaultFunctions(), DefaultTypes(), DefaultTagFields())
 	if err != nil {
 		t.Error(err)
 	}

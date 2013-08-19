039bfcab68333813649622bda07682bab4506828
Alberto García Hierro <alberto@garciahierro.com>
2013-08-19 16:28:09 +0200

    Fix recalculation of request arguments

--- mux/provider.go
+++ mux/provider.go
@@ -26,26 +26,24 @@ type regexpProvider struct {
 }
 
 func (r *regexpProvider) buildArguments() {
-	var arguments []string
 	n := r.re.NumSubexp() + 1
 	m := r.matches
 	for ii := 0; ii < n; ii++ {
 		if x := 2 * ii; x < len(m) && m[x] >= 0 {
-			arguments = append(arguments, r.path[m[x]:m[x+1]])
+			r.arguments = append(r.arguments, r.path[m[x]:m[x+1]])
 		}
 	}
-	r.arguments = arguments
 }
 
 func (r *regexpProvider) Count() int {
-	if r.arguments == nil {
+	if len(r.arguments) == 0 {
 		r.buildArguments()
 	}
 	return len(r.arguments) - 1
 }
 
 func (r *regexpProvider) Arg(idx int) string {
-	if r.arguments == nil {
+	if len(r.arguments) == 0 {
 		r.buildArguments()
 	}
 	if idx < len(r.arguments)-1 {
@@ -55,7 +53,7 @@ func (r *regexpProvider) Arg(idx int) string {
 }
 
 func (r *regexpProvider) Param(name string) string {
-	if r.arguments == nil {
+	if len(r.arguments) == 0 {
 		r.buildArguments()
 	}
 	for ii, v := range r.re.SubexpNames() {
@@ -73,5 +71,5 @@ func (r *regexpProvider) reset(re *regexp.Regexp, path string, matches []int) {
 	r.re = re
 	r.path = path
 	r.matches = matches
-	r.arguments = r.arguments[:]
+	r.arguments = r.arguments[:0]
 }

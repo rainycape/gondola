3e7c48ce770fbe1cf40eedab79f82f9952a78799
Alberto García Hierro <alberto@garciahierro.com>
2013-09-01 17:00:30 +0200

    Optimize the json codec to minimize the number of function calls
    Make json.Marshal and json.Unmarshal the Encode and Decode functions.
    This avoids a few allocations and a function call for every time
    the codec is used.

--- cache/codec/json.go
+++ cache/codec/json.go
@@ -1,25 +1,12 @@
 package codec
 
 import (
-	"bytes"
 	"encoding/json"
 )
 
 var (
-	JsonCodec = &Codec{Encode: jsonMarshal, Decode: jsonUnmarshal}
+	JsonCodec = &Codec{Encode: json.Marshal, Decode: json.Unmarshal}
 )
-
-func jsonMarshal(v interface{}) ([]byte, error) {
-	var buf bytes.Buffer
-	if err := json.NewEncoder(&buf).Encode(v); err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-
-func jsonUnmarshal(data []byte, v interface{}) error {
-	return json.NewDecoder(bytes.NewBuffer(data)).Decode(v)
-}
 
 func init() {
 	Register("json", JsonCodec)

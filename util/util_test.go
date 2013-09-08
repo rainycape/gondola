e09fd7211e176699414ea99e7dd564559275d63d
Alberto García Hierro <alberto@garciahierro.com>
2013-09-08 14:15:31 +0200

    Add test cases for UnCamelCase

--- util/util_test.go
+++ util/util_test.go
@@ -24,15 +24,16 @@ func TestSlug(t *testing.T) {
 
 func TestCamelCaseToLower(t *testing.T) {
 	cases := map[string]string{
-		"FooBarBaz": "foo_bar_baz",
-		"FOOBarBaz": "foo_bar_baz",
-		"TEST":      "test",
-		"goLANG":    "go_lang",
-		"":          "",
-		"T":         "t",
-		"t":         "t",
-		"Id":        "id",
-		"FóoBar":    "fóo_bar",
+		"FooBarBaz":  "foo_bar_baz",
+		"FOOBarBaz":  "foo_bar_baz",
+		"TEST":       "test",
+		"goLANG":     "go_lang",
+		"myINTValue": "my_int_value",
+		"":           "",
+		"T":          "t",
+		"t":          "t",
+		"Id":         "id",
+		"FóoBar":     "fóo_bar",
 	}
 	for k, v := range cases {
 		if u := CamelCaseToLower(k, "_"); u != v {

3426bd7316fafd5f183b22c73946565bc56e5e8a
Alberto García Hierro <alberto@garciahierro.com>
2013-09-11 05:22:06 +0200

    Avoid crashing when the value is not valid.

--- defaults/config.go
+++ defaults/config.go
@@ -4,17 +4,33 @@ import (
 	"reflect"
 )
 
+func isBool(v reflect.Value) bool {
+	if !v.IsValid() {
+		return false
+	}
+	return v.Kind() == reflect.Bool
+}
+
 func isInt(v reflect.Value) bool {
+	if !v.IsValid() {
+		return false
+	}
 	k := v.Type().Kind()
 	return k == reflect.Int || k == reflect.Int8 || k == reflect.Int16 || k == reflect.Int32 || k == reflect.Int64
 }
 
 func isUint(v reflect.Value) bool {
+	if !v.IsValid() {
+		return false
+	}
 	k := v.Type().Kind()
 	return k == reflect.Uint || k == reflect.Uint8 || k == reflect.Uint16 || k == reflect.Uint32 || k == reflect.Uint64
 }
 
 func isNonEmptyString(v reflect.Value) bool {
+	if !v.IsValid() {
+		return false
+	}
 	if v.Type().Kind() == reflect.String {
 		return v.String() != ""
 	}
@@ -23,7 +39,7 @@ func isNonEmptyString(v reflect.Value) bool {
 
 func setStringDefault(val reflect.Value, name string, f func(string)) {
 	value := val.FieldByName(name)
-	if value.IsValid() && value.String() != "" {
+	if isNonEmptyString(value) {
 		f(value.String())
 	}
 }
@@ -33,7 +49,7 @@ func setDefaults(object interface{}) {
 	for val.Type().Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
-	if debug := val.FieldByName("Debug"); debug.Type().Kind() == reflect.Bool {
+	if debug := val.FieldByName("Debug"); isBool(debug) {
 		SetDebug(debug.Bool())
 	}
 	if port := val.FieldByName("Port"); isInt(port) || isUint(port) {

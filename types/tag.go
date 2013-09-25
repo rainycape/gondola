985412ac8e4b842e1ea13e47745f44510535098e
Alberto García Hierro <alberto@garciahierro.com>
2013-09-25 19:58:23 +0200

    Fix parsing tags with quoted values
    Also, add tests for tag parsing

--- types/tag.go
+++ types/tag.go
@@ -1,8 +1,9 @@
 package types
 
 import (
+	"bytes"
+	"fmt"
 	"reflect"
-	"strings"
 )
 
 type Tag struct {
@@ -63,36 +64,134 @@ func (t *Tag) IsEmpty() bool {
 	return t.name == "" && len(t.values) == 0
 }
 
-func makeTag(tag string) *Tag {
-	fields := strings.Split(tag, ",")
-	name := fields[0]
-	values := make(map[string]string, len(fields)-1)
-	for _, v := range fields[1:] {
-		idx := strings.Index(v, ":")
-		if idx >= 0 {
-			values[v[:idx]] = v[idx+1:]
-		} else {
-			values[v] = ""
+func splitFields(tag string) (string, map[string]string, error) {
+	const (
+		stateKey = iota
+		stateValue
+		stateValueQuoted
+		stateEscape
+	)
+	hasName := false
+	var name string
+	var key string
+	var prevState int
+	state := stateKey
+	var buf bytes.Buffer
+	values := make(map[string]string)
+	for ii, v := range []byte(tag) {
+		switch v {
+		case ',':
+			if state != stateValueQuoted {
+				if state == stateEscape {
+					return "", nil, fmt.Errorf("unknown escape sequence \\%s at %d", string(v), ii)
+				}
+				if hasName {
+					if key != "" {
+						values[key] = buf.String()
+						key = ""
+					} else {
+						values[buf.String()] = ""
+					}
+				} else {
+					name = buf.String()
+					hasName = true
+				}
+				buf.Reset()
+				state = stateKey
+			} else {
+				buf.WriteByte(v)
+			}
+		case '\'':
+			if state == stateValue {
+				if buf.Len() == 0 {
+					state = stateValueQuoted
+				} else {
+					buf.WriteByte(v)
+				}
+			} else if state == stateValueQuoted {
+				values[key] = buf.String()
+				key = ""
+				buf.Reset()
+				state = stateKey
+			} else if state == stateEscape {
+				buf.WriteByte(v)
+				state = prevState
+			} else {
+				return "", nil, fmt.Errorf("illegal character ' in key at %d", ii)
+			}
+		case '\\':
+			if state == stateEscape {
+				buf.WriteByte(v)
+			} else {
+				prevState = state
+				state = stateEscape
+			}
+		case ':':
+			if state == stateKey {
+				key = buf.String()
+				buf.Reset()
+				state = stateValue
+			} else {
+				buf.WriteByte(v)
+			}
+		default:
+			if state == stateEscape {
+				return "", nil, fmt.Errorf("unknown escape sequence \\%s at %d", string(v), ii)
+			}
+			buf.WriteByte(v)
 		}
 	}
-	return &Tag{name, values}
+	switch state {
+	case stateKey:
+		if k := buf.String(); k != "" {
+			if hasName {
+				values[k] = ""
+			} else {
+				name = k
+			}
+		}
+	case stateValue:
+		values[key] = buf.String()
+	default:
+		return "", nil, fmt.Errorf("unexpected end at %d", len(tag))
+	}
+	return name, values, nil
+}
+
+// ParseTag parses a Gondola style struct tag field from
+// the given tag string.
+func ParseTag(tag string) (*Tag, error) {
+	name, values, err := splitFields(tag)
+	if err != nil {
+		return nil, err
+	}
+	return &Tag{name, values}, nil
+}
+
+// MustParseTag works like ParseTag, but panics if there's an error.
+func MustParseTag(tag string) *Tag {
+	t, err := ParseTag(tag)
+	if err != nil {
+		panic(err)
+	}
+	return t
 }
 
 func NewTag(field reflect.StructField, alternatives []string) *Tag {
 	for _, v := range alternatives {
 		t := field.Tag.Get(v)
 		if t != "" {
-			return makeTag(t)
+			return MustParseTag(t)
 		}
 	}
-	return makeTag("")
+	return MustParseTag("")
 }
 
 func NewTagNamed(field reflect.StructField, name string) *Tag {
-	return makeTag(field.Tag.Get(name))
+	return MustParseTag(field.Tag.Get(name))
 }
 
 func NewStringTagNamed(tag string, name string) *Tag {
 	t := reflect.StructTag(tag)
-	return makeTag(t.Get(name))
+	return MustParseTag(t.Get(name))
 }
---
+++ types/tag_test.go
@@ -0,0 +1,68 @@
+package types
+
+import (
+	"reflect"
+	"testing"
+)
+
+type tagCase struct {
+	tag   string
+	value *Tag
+	err   string
+}
+
+var (
+	cases = []tagCase{
+		{
+			",label:Message,help:'Please, enter a message'",
+			&Tag{name: "", values: map[string]string{"label": "Message", "help": "Please, enter a message"}},
+			"",
+		},
+		{
+			"foo,label:bar,help:let's rock",
+			&Tag{name: "foo", values: map[string]string{"label": "bar", "help": "let's rock"}},
+			"",
+		},
+		{
+			"foo,label:bar,help:'let\\'s rock'",
+			&Tag{name: "foo", values: map[string]string{"label": "bar", "help": "let's rock"}},
+			"",
+		},
+		{
+			"-",
+			&Tag{name: "-", values: map[string]string{}},
+			"",
+		},
+		{
+			",inv'alidkey",
+			nil,
+			"illegal character ' in key at 4",
+		},
+		{
+			"inv'alidkey",
+			nil,
+			"illegal character ' in key at 3",
+		},
+	}
+)
+
+func TestParseTags(t *testing.T) {
+	for _, v := range cases {
+		tag, err := ParseTag(v.tag)
+		if err != nil {
+			if v.err == "" {
+				t.Error(err)
+			} else {
+				if v.err != err.Error() {
+					t.Errorf("expecting error %v, got %v", v.err, err)
+				}
+			}
+		} else {
+			if v.err != "" {
+				t.Errorf("expecting error %v, got value %v", v.err, tag)
+			} else if !reflect.DeepEqual(v.value, tag) {
+				t.Errorf("different tags: want %v, got %v", v.value, tag)
+			}
+		}
+	}
+}

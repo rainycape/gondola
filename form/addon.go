c80bdab0ce04cd4fbb19c7afe0f691d2bab0c597
Alberto García Hierro <alberto@garciahierro.com>
2013-09-26 21:10:39 +0200

    Document addon.go and choices.go

--- form/addon.go
+++ form/addon.go
@@ -5,18 +5,34 @@ import (
 	"gnd.la/mux"
 )
 
+// AddOnPosition indicates the position of the addon
+// relative to the field input.
 type AddOnPosition int
 
 const (
+	// Before the input
 	AddOnPositionBefore = iota
+	// After the input
 	AddOnPositionAfter
 )
 
+// AddOn represents an addon which can be added
+// to a form field. Keep in mind that not all
+// renderers nor all types of fields support
+// addons. Check each renderer's documentation
+// for further information.
 type AddOn struct {
-	Node     *html.Node
+	// The HTML node to use as addon
+	Node *html.Node
+	// The addon position
 	Position AddOnPosition
 }
 
+// AddOnProvider is an interface which might be implemented
+// by types included in a form. If the type implements this
+// interface, its function will be called for every field.
 type AddOnProvider interface {
+	// FieldAddOns returns the addons for the given field.
+	// It's called just before the field is rendered.
 	FieldAddOns(ctx *mux.Context, field *Field) []*AddOn
 }
--- form/choices.go
+++ form/choices.go
@@ -4,11 +4,19 @@ import (
 	"gnd.la/mux"
 )
 
+// Choice represents a choice in a select or radio field.
 type Choice struct {
 	Name  string
 	Value interface{}
 }
 
+// ChoicesProvider is the interface implemented by types
+// which contain a field or type select or radio. The
+// function will only be called for fields of those types.
+// If a type which contains choices does not implement this
+// interface, the form will panic on creation.
 type ChoicesProvider interface {
+	// FieldChoices returns the choices for the given field.
+	// It's called just before the field is rendered.
 	FieldChoices(ctx *mux.Context, field *Field) []*Choice
 }

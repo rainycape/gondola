a036a24d1a8548524f25f02c3d4a30817bd7bf6f
Alberto García Hierro <alberto@garciahierro.com>
2013-10-09 15:24:33 +0200

    Add packages for checking links counts on gplus and twitter

---
+++ social/gplus/stats.go
@@ -0,0 +1,107 @@
+package gplus
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+var (
+	rpc                = "https://clients6.google.com/rpc?key=AIzaSyCKSbrvQasunBoV16zDH9R33D88CeLr9gQ"
+	errInvalidResponse = errors.New("invalid JSON response")
+)
+
+type LinkStats struct {
+	Normalized string
+	Count      int
+}
+
+func GetLinkStats(url string) (*LinkStats, error) {
+	if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+		url = "http://" + url
+	}
+	body := fmt.Sprintf(`[{
+	    "method":"pos.plusones.get",
+	    "id":"p",
+	    "params":{
+		"nolog":true,
+		"id":"%s",
+		"source":"widget",
+		"userId":"@viewer",
+		"groupId":"@self"
+	    },
+	    "jsonrpc":"2.0",
+	    "key":"p",
+	    "apiVersion":"v1"
+	}]`, url)
+	resp, err := http.Post(rpc, "application/json", strings.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var res []map[string]interface{}
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&res); err != nil {
+		return nil, err
+	}
+	if len(res) != 1 {
+		return nil, errInvalidResponse
+	}
+	result, ok := res[0]["result"].(map[string]interface{})
+	if !ok {
+		return nil, errInvalidResponse
+	}
+	normalized, ok := result["id"].(string)
+	if !ok {
+		return nil, errInvalidResponse
+	}
+	metadata, ok := result["metadata"].(map[string]interface{})
+	if !ok {
+		return nil, errInvalidResponse
+	}
+	counts, ok := metadata["globalCounts"].(map[string]interface{})
+	if !ok {
+		return nil, errInvalidResponse
+	}
+	count, ok := counts["count"].(float64)
+	if !ok {
+		return nil, errInvalidResponse
+	}
+	return &LinkStats{
+		Normalized: normalized,
+		Count:      int(count),
+	}, nil
+}
+
+type result struct {
+	url   string
+	stats *LinkStats
+	err   error
+}
+
+func GetLinksStats(urls []string) (map[string]*LinkStats, error) {
+	count := len(urls)
+	ch := make(chan *result, count)
+	for _, v := range urls {
+		go func(u string) {
+			stats, err := GetLinkStats(u)
+			ch <- &result{
+				url:   u,
+				stats: stats,
+				err:   err,
+			}
+		}(v)
+	}
+	results := make(map[string]*LinkStats, count)
+	for ii := 0; ii < len(urls); ii++ {
+		res := <-ch
+		if res.err != nil {
+			return nil, res.err
+		}
+		results[res.url] = res.stats
+	}
+	close(ch)
+	return results, nil
+}
---
+++ social/twitter/stats.go
@@ -0,0 +1,78 @@
+package twitter
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+var (
+	endPoint           = "http://urls.api.twitter.com/1/urls/count.json?url="
+	errInvalidResponse = errors.New("invalid JSON response")
+)
+
+type LinkStats struct {
+	Normalized string
+	Count      int
+}
+
+func GetLinkStats(u string) (*LinkStats, error) {
+	if !strings.HasPrefix(u, "http://") && !strings.HasPrefix(u, "https://") {
+		u = "http://" + u
+	}
+	resp, err := http.Get(endPoint + url.QueryEscape(u))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	var res map[string]interface{}
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&res); err != nil {
+		return nil, err
+	}
+	normalized, ok := res["url"].(string)
+	if !ok {
+		return nil, errInvalidResponse
+	}
+	count, ok := res["count"].(float64)
+	if !ok {
+		return nil, errInvalidResponse
+	}
+	return &LinkStats{
+		Normalized: normalized,
+		Count:      int(count),
+	}, nil
+}
+
+type result struct {
+	url   string
+	stats *LinkStats
+	err   error
+}
+
+func GetLinksStats(urls []string) (map[string]*LinkStats, error) {
+	count := len(urls)
+	ch := make(chan *result, count)
+	for _, v := range urls {
+		go func(u string) {
+			stats, err := GetLinkStats(u)
+			ch <- &result{
+				url:   u,
+				stats: stats,
+				err:   err,
+			}
+		}(v)
+	}
+	results := make(map[string]*LinkStats, count)
+	for ii := 0; ii < len(urls); ii++ {
+		res := <-ch
+		if res.err != nil {
+			return nil, res.err
+		}
+		results[res.url] = res.stats
+	}
+	close(ch)
+	return results, nil
+}

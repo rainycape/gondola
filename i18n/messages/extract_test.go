d8d5fc21527b1a047bcf5fff7b9213cadef5d622
Alberto García Hierro <alberto@garciahierro.com>
2013-10-22 20:06:48 +0200

    Update test for message extraction

--- i18n/messages/extract_test.go
+++ i18n/messages/extract_test.go
@@ -1,8 +1,10 @@
 package messages
 
 import (
+	"bytes"
 	"gnd.la/log"
-	"os"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 )
 
@@ -12,6 +14,16 @@ func TestExtract(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	t.Logf("Messages %v", m)
-	Write(os.Stdout, m)
+	var buf bytes.Buffer
+	if err := Write(&buf, m); err != nil {
+		t.Error(err)
+	}
+	t.Logf("Messages:\n%s", string(buf.Bytes()))
+	b, err := ioutil.ReadFile(filepath.Join("_test_data", "test.pot"))
+	if err != nil {
+		t.Error(err)
+	}
+	if len(b) != len(buf.Bytes()) || bytes.Compare(b, buf.Bytes()) != 0 {
+		t.Errorf("invalid messages (%d / %d)", len(b), len(buf.Bytes()))
+	}
 }

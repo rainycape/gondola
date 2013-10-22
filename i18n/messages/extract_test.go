8d735d3a1a00c11b5eff500b5a50a69e9e6240ce
Alberto García Hierro <alberto@garciahierro.com>
2013-10-22 20:09:17 +0200

    Don't set the logging level explicitely

--- i18n/messages/extract_test.go
+++ i18n/messages/extract_test.go
@@ -2,14 +2,12 @@ package messages
 
 import (
 	"bytes"
-	"gnd.la/log"
 	"io/ioutil"
 	"path/filepath"
 	"testing"
 )
 
 func TestExtract(t *testing.T) {
-	log.SetLevel(log.LDebug)
 	m, err := Extract("_test_data", DefaultFunctions(), DefaultTypes(), DefaultTagFields())
 	if err != nil {
 		t.Error(err)

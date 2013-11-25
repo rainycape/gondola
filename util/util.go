f4abaae41ad13a91578d4d802f09a8c2083a115c
Alberto García Hierro <alberto@garciahierro.com>
2013-11-25 14:16:21 +0100

    Add shorthand type M

--- util/util.go
+++ util/util.go
@@ -1,3 +1,7 @@
 // Package util provides several utility functions
 // which are commonly used while building web apps
 package util
+
+// M is an alias for map[string]interface{}, provided
+// as a shorthand.
+type M map[string]interface{}

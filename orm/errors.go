193459fa6c59fd6627b82a6c74c37733cb4bce87
Alberto García Hierro <alberto@garciahierro.com>
2013-11-28 22:46:49 +0100

    Document errors.

--- orm/errors.go
+++ orm/errors.go
@@ -5,6 +5,8 @@ import (
 )
 
 var (
+	// ErrNotFound is returned from One() when there are no results.
 	ErrNotFound = errors.New("no results found")
-	ErrNoSql    = errors.New("driver is not using database/sql")
+	// ErrNotSql indicates that the current driver is not using database/sql.
+	ErrNoSql = errors.New("driver is not using database/sql")
 )

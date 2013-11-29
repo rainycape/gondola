5226be5530db86bfd954a37cb0c22d7fe81c7584
Alberto García Hierro <alberto@garciahierro.com>
2013-11-29 19:11:55 +0100

    Add BackgroundContext()

---
+++ mux/discard.go
@@ -0,0 +1,24 @@
+package mux
+
+import (
+	"net/http"
+)
+
+var discard = &discarder{}
+
+// discarded is used by background context
+// to avoid crashing if a background task
+// writes to the context.
+type discarder struct {
+}
+
+func (d *discarder) Header() http.Header {
+	return make(http.Header)
+}
+
+func (d *discarder) Write(b []byte) (int, error) {
+	return len(b), nil
+}
+
+func (d *discarder) WriteHeader(_ int) {
+}

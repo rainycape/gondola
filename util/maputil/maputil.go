fa4a358d0d858a0ff30123e31e221cb353a871cd
Alberto García Hierro <alberto@garciahierro.com>
2013-12-08 08:43:44 +0000

    Add gnd.la/util/maputil
    Includes Keys() and Values() conveniency functions

---
+++ util/maputil/maputil.go
@@ -0,0 +1,76 @@
+// Package maputil constains utility functions for working with maps.
+//
+// Keep in mind that these functions use reflection internally, so they're
+// around 5x times slower than non-reflect code
+package maputil
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Keys returns the keys of the map m in the pointer to slice out.
+// The key type of the map must match the element type of the slice.
+// Any type mismatch will result in a panic, since it's a programming
+// error.
+func Keys(m interface{}, out interface{}) {
+	if err := keys(m, out); err != nil {
+		panic(err)
+	}
+}
+
+func keys(m interface{}, out interface{}) error {
+	mval := reflect.ValueOf(m)
+	mt := mval.Type()
+	if mt.Kind() != reflect.Map {
+		return fmt.Errorf("first argument to Keys() must be map, not %T", m)
+	}
+	oval := reflect.ValueOf(out)
+	ot := oval.Type()
+	if ot.Kind() != reflect.Ptr || ot.Elem().Kind() != reflect.Slice {
+		return fmt.Errorf("second argument to Keys() must be pointer to slice, not %T", out)
+	}
+	if mt.Key() != ot.Elem().Elem() {
+		return fmt.Errorf("map key type and slice element type must match, but instead they're %s and %s", mt.Key(), ot.Elem().Elem())
+	}
+	count := mval.Len()
+	ks := reflect.MakeSlice(oval.Type().Elem(), count, count)
+	for ii, v := range mval.MapKeys() {
+		ks.Index(ii).Set(v)
+	}
+	oval.Elem().Set(ks)
+	return nil
+}
+
+// Values returns the values of the map m in the pointer to slice out.
+// The value type of the map must match the element type of the slice.
+// Any type mismatch will result in a panic, since it's a programming
+// error.
+func Values(m interface{}, out interface{}) {
+	if err := values(m, out); err != nil {
+		panic(err)
+	}
+}
+
+func values(m interface{}, out interface{}) error {
+	mval := reflect.ValueOf(m)
+	mt := mval.Type()
+	if mt.Kind() != reflect.Map {
+		return fmt.Errorf("first argument to Values() must be map, not %T", m)
+	}
+	oval := reflect.ValueOf(out)
+	ot := oval.Type()
+	if ot.Kind() != reflect.Ptr || ot.Elem().Kind() != reflect.Slice {
+		return fmt.Errorf("second argument to Values() must be pointer to slice, not %T", out)
+	}
+	if mt.Elem() != ot.Elem().Elem() {
+		return fmt.Errorf("map element type and slice element type must match, but instead they're %s and %s", mt.Elem(), ot.Elem().Elem())
+	}
+	count := mval.Len()
+	ks := reflect.MakeSlice(oval.Type().Elem(), count, count)
+	for ii, v := range mval.MapKeys() {
+		ks.Index(ii).Set(mval.MapIndex(v))
+	}
+	oval.Elem().Set(ks)
+	return nil
+}
---
+++ util/maputil/maputil_test.go
@@ -0,0 +1,51 @@
+package maputil
+
+import (
+	"sort"
+	"testing"
+)
+
+const count = 1000 * 1000
+
+var dict map[int]int
+
+func testMap(t *testing.T, f func(interface{}, interface{})) {
+	var out []int
+	f(dict, &out)
+	if len(out) != len(dict) {
+		t.Fatalf("expecting %d elements, got %d instead", len(dict), len(out))
+	}
+	sort.Ints(out)
+	for ii := 0; ii < count; ii++ {
+		if out[ii] != ii {
+			t.Errorf("bad value at index %d: %d", ii, out[ii])
+		}
+	}
+}
+
+func TestKeys(t *testing.T) {
+	testMap(t, Keys)
+}
+
+func TestValues(t *testing.T) {
+	testMap(t, Values)
+}
+
+func BenchmarkKeys(b *testing.B) {
+	var out []int
+	Keys(dict, &out)
+}
+
+func BenchmarkKeysNoReflect(b *testing.B) {
+	out := make([]int, 0, len(dict))
+	for k := range dict {
+		out = append(out, k)
+	}
+}
+
+func init() {
+	dict = make(map[int]int)
+	for ii := 0; ii < count; ii++ {
+		dict[ii] = ii
+	}
+}

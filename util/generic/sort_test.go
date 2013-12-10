f1a3d4c92deab8889e10c868ddc046324d355bda
Alberto García Hierro <alberto@garciahierro.com>
2013-12-10 08:17:28 +0000

    Add benchmark for sorting
    This way we can easily measure the overhead

--- util/generic/sort_test.go
+++ util/generic/sort_test.go
@@ -1,6 +1,7 @@
 package generic
 
 import (
+	"sort"
 	"testing"
 )
 
@@ -26,7 +27,7 @@ var (
 		{"D", 0},
 		{"A", 1},
 	}
-	numbers []int
+	long []*Test1
 )
 
 func TestField(t *testing.T) {
@@ -68,5 +69,48 @@ func TestMethod(t *testing.T) {
 		if ex := string(chars[len(chars)-ii-1]); ex != v.Name {
 			t.Errorf("bad value at index %d. want %s, got %s", ii, ex, v.Name)
 		}
+	}
+}
+
+func BenchmarkLong(b *testing.B) {
+	b.ReportAllocs()
+	for ii := 0; ii < b.N; ii++ {
+		b.StopTimer()
+		var s []*Test1
+		s = append(s, long...)
+		b.StartTimer()
+		Sort(s, "Value")
+	}
+}
+
+type test1Slice []*Test1
+
+func (t test1Slice) Len() int {
+	return len(t)
+}
+
+func (t test1Slice) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
+
+func (t test1Slice) Less(i, j int) bool {
+	return t[i].Value < t[j].Value
+}
+
+func BenchmarkLongNonReflect(b *testing.B) {
+	b.ReportAllocs()
+	for ii := 0; ii < b.N; ii++ {
+		b.StopTimer()
+		var s []*Test1
+		s = append(s, long...)
+		b.StartTimer()
+		sort.Sort(test1Slice(s))
+	}
+}
+
+func init() {
+	long = make([]*Test1, 1000)
+	for ii := range long {
+		long[ii] = &Test1{Value: len(long) - ii}
 	}
 }

30e4cd331e82ee41b058ff6f7e3993177c410afd
Alberto García Hierro <alberto@garciahierro.com>
2013-12-21 17:22:41 +0000

    Add ParseSize()

--- util/util_test.go
+++ util/util_test.go
@@ -41,3 +41,22 @@ func TestCamelCaseToLower(t *testing.T) {
 		}
 	}
 }
+
+func TestParseSize(t *testing.T) {
+	sizes := map[string]uint64{
+		"0":     0,
+		"0GB":   0,
+		"0.5K":  512,
+		"1.5MB": uint64(1024 * 1024 * 1.5),
+	}
+	for k, v := range sizes {
+		val, err := ParseSize(k)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+		if val != v {
+			t.Errorf("error parsing %q - want %d, got %d", k, v, val)
+		}
+	}
+}

1c51d2dbcc1e7bf72b554e4eaf5e5d54e5183fb1
Alberto García Hierro <alberto@garciahierro.com>
2013-12-24 19:02:32 +0000

    Make the *Table the 1st argument to Operate()
    This way it looks closer to an UPDATE table...

--- orm/operation.go
+++ orm/operation.go
@@ -5,12 +5,12 @@ import (
 	"gnd.la/orm/query"
 )
 
-func (o *Orm) Operate(q query.Q, table *Table, op *operation.Operation) (Result, error) {
+func (o *Orm) Operate(table *Table, q query.Q, op *operation.Operation) (Result, error) {
 	return o.conn.Operate(table.model, q, op)
 }
 
-func (o *Orm) MustOperate(q query.Q, table *Table, op *operation.Operation) Result {
-	res, err := o.Operate(q, table, op)
+func (o *Orm) MustOperate(table *Table, q query.Q, op *operation.Operation) Result {
+	res, err := o.Operate(table, q, op)
 	if err != nil {
 		panic(err)
 	}

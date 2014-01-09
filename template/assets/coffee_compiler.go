0a53e74b1beab4adb6d35c0457f398a858aa2ae0
Alberto García Hierro <alberto@garciahierro.com>
2014-01-09 06:02:50 +0000

    Add support for compiling coffescript

---
+++ template/assets/coffee_compiler.go
@@ -0,0 +1,33 @@
+package assets
+
+import (
+	"io"
+	"os/exec"
+)
+
+var (
+	coffeePath, _ = exec.LookPath("coffee")
+)
+
+type coffeeCompiler struct {
+}
+
+func (c *coffeeCompiler) Compile(w io.Writer, r io.Reader, opts Options) error {
+	if coffeePath != "" {
+		return command(coffeePath, []string{"-sc"}, w, r, opts)
+	}
+	_, _, err := reducer("coffee", w, r)
+	return err
+}
+
+func (c *coffeeCompiler) Type() Type {
+	return TypeJavascript
+}
+
+func (c *coffeeCompiler) Ext() string {
+	return "coffee"
+}
+
+func init() {
+	RegisterCompiler(&coffeeCompiler{})
+}
--- template/assets/reducer.go
+++ template/assets/reducer.go
@@ -14,6 +14,7 @@ import (
 //  Reducer + "css"
 //  Reducer + "js"
 //  Reducer + "less"
+//  Reducer + "coffee"
 //  ...
 //
 // The code to reduce or compile will be sent in

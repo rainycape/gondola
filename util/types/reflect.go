1bca953c9b1aeb5c2e37f549bfdd11c115f52155
Alberto García Hierro <alberto@garciahierro.com>
2014-01-12 08:50:01 +0000

    Add SortValues()

--- util/types/reflect.go
+++ util/types/reflect.go
@@ -2,7 +2,9 @@ package types
 
 import (
 	"errors"
+	"fmt"
 	"reflect"
+	"sort"
 )
 
 var (
@@ -34,4 +36,47 @@ func IsNumeric(typ reflect.Type) bool {
 		k == reflect.Int32 || k == reflect.Uint32 ||
 		k == reflect.Int64 || k == reflect.Uint64 ||
 		k == reflect.Complex128 || k == reflect.Complex64
+}
+
+type rvs []reflect.Value
+
+func (x rvs) Len() int      { return len(x) }
+func (x rvs) Swap(i, j int) { x[i], x[j] = x[j], x[i] }
+
+type rvInts struct{ rvs }
+
+func (x rvInts) Less(i, j int) bool { return x.rvs[i].Int() < x.rvs[j].Int() }
+
+type rvUints struct{ rvs }
+
+func (x rvUints) Less(i, j int) bool { return x.rvs[i].Uint() < x.rvs[j].Uint() }
+
+type rvFloats struct{ rvs }
+
+func (x rvFloats) Less(i, j int) bool { return x.rvs[i].Float() < x.rvs[j].Float() }
+
+type rvStrings struct{ rvs }
+
+func (x rvStrings) Less(i, j int) bool { return x.rvs[i].String() < x.rvs[j].String() }
+
+// SortValues sorts the given slice of reflect.Value, if
+// possible. If the slice can't be sorted, an error is
+// returned.
+func SortValues(v []reflect.Value) error {
+	if len(v) <= 1 {
+		return nil
+	}
+	switch v[0].Kind() {
+	case reflect.Float32, reflect.Float64:
+		sort.Sort(rvFloats{v})
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		sort.Sort(rvInts{v})
+	case reflect.String:
+		sort.Sort(rvStrings{v})
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		sort.Sort(rvUints{v})
+	default:
+		return fmt.Errorf("can't sort %T", v[0].Interface())
+	}
+	return nil
 }

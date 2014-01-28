88ed14dfff64de3286b5c5e5563b21af9cb3bbe3
Alberto García Hierro <alberto@garciahierro.com>
2014-01-28 01:28:17 +0000

    Improve RelativePath, make in cross-platform and work from go test
    Use path/filepath rather than path.
    Use filepath.FromSlash to convert Unix style paths when running on
    Windows.
    When running from go test, return the path relative to the working
    directory,

--- util/path.go
+++ util/path.go
@@ -2,7 +2,12 @@ package util
 
 import (
 	"os"
-	"path"
+	"path/filepath"
+	"strings"
+)
+
+var (
+	inTest bool
 )
 
 // RelativePath returns the given path
@@ -11,14 +16,29 @@ import (
 // binary is at /home/fiam/example/example
 // RelativePath("foo") returns /home/fiam/example/foo
 // RelativePath("foo/bar") returns /home/fiam/example/foo/bar
-// RelativePath("/foo/bar") returns /home/fiam/example/foo/bar
+// RelativePath("/foo/bar") returns /home/fiam/example/foo/bar.
+// Note that when running tests (from e.g. go test), this function
+// will return the path relative to the current directory rather
+// than the binary. This is done in order to allow functions which
+// use relative paths to work while being tested, since go test puts
+// the test binary in a temporary directory, but runs it in the
+// package directory.
 func RelativePath(name string) string {
-	wd, _ := os.Getwd()
 	var full string
-	if path.IsAbs(os.Args[0]) {
-		full = path.Join(path.Dir(os.Args[0]), name)
+	rel := filepath.FromSlash(name)
+	wd, _ := os.Getwd()
+	if inTest {
+		full = filepath.Join(wd, rel)
 	} else {
-		full = path.Join(wd, path.Dir(os.Args[0]), name)
+		if filepath.IsAbs(os.Args[0]) && !inTest {
+			full = filepath.Join(filepath.Dir(os.Args[0]), rel)
+		} else {
+			full = filepath.Join(wd, filepath.Dir(os.Args[0]), rel)
+		}
 	}
-	return path.Clean(full)
+	return filepath.Clean(full)
+}
+
+func init() {
+	inTest = strings.Contains(os.Args[0], string(filepath.Separator)+"_test"+string(filepath.Separator))
 }

5b5841f5b31419c538e56916108acef35fcf5cc0
Alberto García Hierro <alberto@garciahierro.com>
2014-02-01 03:31:04 +0000

    Add FormatSize()

--- util/size.go
+++ util/size.go
@@ -53,3 +53,22 @@ func ParseSize(s string) (uint64, error) {
 	}
 	return uint64(val * mult), nil
 }
+
+// FormatSize returns the given size in bytes formatted as a
+// human readable string. The precision and unit will vary
+// depending on the size.
+func FormatSize(s uint64) string {
+	if s < 1024 {
+		return fmt.Sprintf("%d bytes", s)
+	}
+	if s < 1024*1024 {
+		return fmt.Sprintf("%.1f KB", float64(s)/1024)
+	}
+	if s < 1024*1024*1024 {
+		return fmt.Sprintf("%.2f MB", float64(s)/(1024*1024))
+	}
+	if s < 1024*1024*1024*1024 {
+		return fmt.Sprintf("%.3f GB", float64(s)/(1024*1024*1024))
+	}
+	return fmt.Sprintf("%.4f TB", float64(s)/(1024*1024*1024*1024))
+}

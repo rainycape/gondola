2fb4305eceb1d82430c658563bbb5cc9bb67c3f0
Alberto García Hierro <alberto@garciahierro.com>
2014-03-31 07:33:02 +0100

    Allow to in SendMail to be a string or a []string

--- app/mail.go
+++ app/mail.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"gnd.la/mail"
 	"io"
 )
@@ -28,11 +29,14 @@ func (c *Context) MailTemplate(template string) (mail.Template, error) {
 	return &mailTemplate{tmpl: t.(*tmpl), ctx: c}, nil
 }
 
-// SendMail is a shorthand function for sending an email from a template. If you need
-// more granularity you can use Context.MailTemplate and gnd.la/mail.Send directly.
-// Note that fields other than Message and Data in the opts argument are not altered
-// and are passed as is to gnd.la/mail.Send.
-func (c *Context) SendMail(to []string, template string, data interface{}, opts *mail.Options) error {
+// SendMail is a shorthand function for sending an email from a template.
+// The to parameter might be either a []string or a string, in the latter case
+// it's parsed with gnd.la/mail.ParseAddressList.
+//
+// If you need more granularity you can use Context.MailTemplate and gnd.la/mail.Send
+// directly. Note that fields other than Message and Data in the opts argument are not
+// altered and are passed as is to gnd.la/mail.Send.
+func (c *Context) SendMail(to interface{}, template string, data interface{}, opts *mail.Options) error {
 	if template != "" {
 		t, err := c.MailTemplate(template)
 		if err != nil {
@@ -44,11 +48,24 @@ func (c *Context) SendMail(to []string, template string, data interface{}, opts 
 		opts.Message = t
 		opts.Data = data
 	}
-	return mail.Send(to, opts)
+	var addrs []string
+	switch x := to.(type) {
+	case string:
+		p, err := mail.ParseAddressList(x)
+		if err != nil {
+			return err
+		}
+		addrs = p
+	case []string:
+		addrs = x
+	default:
+		return fmt.Errorf("invalid to type %T (%v)", to, to)
+	}
+	return mail.Send(addrs, opts)
 }
 
 // MustSendMail works like SendMail, but panics if there's an error.
-func (c *Context) MustSendMail(to []string, template string, data interface{}, opts *mail.Options) {
+func (c *Context) MustSendMail(to interface{}, template string, data interface{}, opts *mail.Options) {
 	if err := c.SendMail(to, template, data, opts); err != nil {
 		panic(err)
 	}

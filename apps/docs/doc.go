dba1e39b229cc332c7e87ea7dca3f2e0f06993e1
Alberto García Hierro <alberto@garciahierro.com>
2014-04-02 17:58:36 +0100

    Add package doc for the docs app

---
+++ apps/docs/doc.go
@@ -0,0 +1,14 @@
+// Package docs implements a Gondola application for browsing package
+// documentation.
+//
+// This application requires a Go source code checkout to be present
+// on the server as well as GOROOT and GOPATH properly configured.
+// Additionaly, a working Go installation is required to automatically
+// download and update packages.
+//
+// To configure the packages to list in the index, use the Groups
+// variable.
+//
+// This application can also automatically fetch and update the packages
+// listed in the index. See StartUpdatingPackages and StopUpdatingPackages.
+package docs

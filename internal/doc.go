1a411a5e5395e59463ef1e4a1fa3202c3f57d2b9
Alberto García Hierro <alberto@garciahierro.com>
2014-04-03 03:00:22 +0100

    Fix missing words in documentation

--- internal/doc.go
+++ internal/doc.go
@@ -2,7 +2,7 @@
 // used by several parts of Gondola.
 //
 // Users should not use this package nor any or
-// this subpackages, since considered internal to
+// this subpackages, since they're considered internal to
 // Gondola, and only exported to avoid code repetition
 // across several packages. As such, their interface
 // might change at any time without prior notice.

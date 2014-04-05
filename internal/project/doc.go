6db8ce22fbd2aef0f7e58acef447041d583b8153
Alberto García Hierro <alberto@garciahierro.com>
2014-04-05 19:15:19 +0100

    Add internal project pkg

---
+++ internal/project/doc.go
@@ -0,0 +1,6 @@
+// Package project contains functions for managing Gondola projects.
+//
+// This package contains exported types because they're shared by
+// both the gondola command and the gondola website. Users should
+// no use any types nor functions from this package.
+package project
---
+++ internal/project/template.go
@@ -0,0 +1,100 @@
+package project
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"gnd.la/util/yaml"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	metaFile = "_template.yaml"
+)
+
+type Template struct {
+	Name        string `json:"name"`
+	Description string `json:"desc"`
+	URL         string `json:"url"`
+	Version     int    `json:"v"`
+	dir         string
+}
+
+func (t *Template) Data() ([]byte, error) {
+	var buf bytes.Buffer
+	zw, _ := gzip.NewWriterLevel(&buf, gzip.BestCompression)
+	tw := tar.NewWriter(zw)
+	err := filepath.Walk(t.dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		name := strings.TrimPrefix(path, t.dir)
+		if name != "" {
+			if name[0] == filepath.Separator {
+				name = name[1:]
+			}
+			if name == "_template.yaml" {
+				return nil
+			}
+			hdr, err := tar.FileInfoHeader(info, "")
+			if err != nil {
+				return err
+			}
+			if dir, _ := filepath.Split(name); dir != "" {
+				hdr.Name = filepath.ToSlash(filepath.Join(dir, hdr.Name))
+			}
+			if err := tw.WriteHeader(hdr); err != nil {
+				return err
+			}
+			if !info.IsDir() {
+				f, err := os.Open(path)
+				if err != nil {
+					return err
+				}
+				defer f.Close()
+				if _, err := io.Copy(tw, f); err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if err := tw.Close(); err != nil {
+		return nil, err
+	}
+	if err := zw.Close(); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func LoadTemplates(dir string) ([]*Template, error) {
+	dir, err := filepath.Abs(dir)
+	if err != nil {
+		return nil, err
+	}
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	var templates []*Template
+	for _, v := range files {
+		tmplDir := filepath.Join(dir, v.Name())
+		var tmpl Template
+		meta := filepath.Join(tmplDir, metaFile)
+		if err := yaml.UnmarshalFile(meta, &tmpl); err != nil {
+			return nil, err
+		}
+		tmpl.dir = tmplDir
+		tmpl.Name = v.Name()
+		templates = append(templates, &tmpl)
+	}
+	return templates, nil
+}

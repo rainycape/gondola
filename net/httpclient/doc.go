bc026b1587350ce7ca2bc9c992f0b0ef10d72c98
Alberto García Hierro <alberto@garciahierro.com>
2014-04-17 04:23:06 +0100

    Add doc for pkg httpclient

---
+++ net/httpclient/doc.go
@@ -0,0 +1,6 @@
+// Package httpclient provides an HTTP client with several conveniency
+// functions.
+//
+// This package is cross platform, so it works on both standalone deployments
+// as well as on App Engine.
+package httpclient

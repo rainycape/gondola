60630c1f6c3ee55fc45adeb633d28952d950aa72
Alberto García Hierro <alberto@garciahierro.com>
2014-05-03 04:00:52 +0100

    Document that Password uses PBKDF2 internally

--- users/password/doc.go
+++ users/password/doc.go
@@ -2,7 +2,7 @@
 // and checking passwords.
 //
 // Passwords are encoded using a per-password salt and then
-// hashed with the chosen algorithm (sha256 by default).
+// hashed using PBKDF2 with the chosen algorithm (sha256 by default).
 // Password provides the Check() method for verifying that
 // the given plaintext matches the encoded password. This
 // method is not vulnerable to timing attacks.

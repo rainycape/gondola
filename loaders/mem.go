b72e4b5eb933a54d8e086ae95c810643b6530c80
Alberto García Hierro <alberto@garciahierro.com>
2014-05-04 17:20:56 +0100

    Correctly implement List() for memLoader
    Merge the results from the map and the initial loader

--- loaders/mem.go
+++ loaders/mem.go
@@ -17,6 +17,18 @@ func (m *memLoader) Load(name string) (ReadSeekCloser, time.Time, error) {
 	return m.loader.Load(name)
 }
 
+func (m *memLoader) List() ([]string, error) {
+	loaderFiles, err := m.loader.List()
+	if err != nil {
+		return nil, err
+	}
+	memFiles, err := m.mapLoader.List()
+	if err != nil {
+		return nil, err
+	}
+	return append(loaderFiles, memFiles...), nil
+}
+
 // MemLoader wraps another loader, causing
 // the created files to be stored in memory.
 // It's usually used with FSLoader, to avoid

60853cdf23ca1e64fd7b01d9d4a0f415f1f0ad19
Alberto García Hierro <alberto@garciahierro.com>
2014-05-06 18:28:18 +0100

    Fix arguments size in uint{16,32} functions

--- encoding/binary/asm_amd64.s
+++ encoding/binary/asm_amd64.s
@@ -1,24 +1,24 @@
 // +build !appengine
 
-TEXT    ·leUint16(SB),7,$0-32
+TEXT    ·leUint16(SB),7,$0-26
     MOVQ    b+0(FP),AX
     MOVW    (AX),AX
     MOVW    AX,ret+24(FP)
     RET
 
-TEXT    ·lePutUint16(SB),7,$0-32
+TEXT    ·lePutUint16(SB),7,$0-26
     MOVQ    b+0(FP),AX
     MOVW    v+24(FP),BX
     MOVW    BX,(AX)
     RET
 
-TEXT    ·leUint32(SB),7,$0-32
+TEXT    ·leUint32(SB),7,$0-28
     MOVQ    b+0(FP),AX
     MOVL    (AX),AX
     MOVL    AX,ret+24(FP)
     RET
 
-TEXT    ·lePutUint32(SB),7,$0-32
+TEXT    ·lePutUint32(SB),7,$0-28
     MOVQ    b+0(FP),AX
     MOVL    v+24(FP),BX
     MOVL    BX, (AX)
@@ -36,28 +36,28 @@ TEXT    ·lePutUint64(SB),7,$0-32
     MOVQ    BX,(AX)
     RET
 
-TEXT    ·beUint16(SB),7,$0-32
+TEXT    ·beUint16(SB),7,$0-26
     MOVQ    b+0(FP),AX
     MOVL    (AX),AX
     XCHGB   AX,AH
     MOVW    AX,ret+24(FP)
     RET
 
-TEXT    ·bePutUint16(SB),7,$0-32
+TEXT    ·bePutUint16(SB),7,$0-26
     MOVQ    b+0(FP),AX
     MOVW    v+24(FP),BX
     XCHGB   BX,BH
     MOVW    BX,(AX)
     RET
 
-TEXT    ·beUint32(SB),7,$0-32
+TEXT    ·beUint32(SB),7,$0-28
     MOVQ    b+0(FP),AX
     MOVL    (AX),AX
     BSWAPL  AX
     MOVL    AX,ret+24(FP)
     RET
 
-TEXT    ·bePutUint32(SB),7,$0-32
+TEXT    ·bePutUint32(SB),7,$0-28
     MOVQ    b+0(FP),AX
     MOVL    v+24(FP),BX
     BSWAPL  BX

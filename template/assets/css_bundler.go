af819e8db4e198b56639025a9efee24fbff50faa
Alberto García Hierro <alberto@garciahierro.com>
2014-05-14 02:46:04 +0100

    Add support for bundling CSS locally with cleancss

--- template/assets/css_bundler.go
+++ template/assets/css_bundler.go
@@ -1,14 +1,23 @@
 package assets
 
 import (
-	"gnd.la/log"
 	"io"
+	"os/exec"
+
+	"gnd.la/log"
+)
+
+var (
+	cleanCSSPath, _ = exec.LookPath("cleancss")
 )
 
 type cssBundler struct {
 }
 
 func (c *cssBundler) Bundle(w io.Writer, r io.Reader, opts Options) error {
+	if cleanCSSPath != "" {
+		return command(cleanCSSPath, []string{"--s0"}, w, r, opts)
+	}
 	p, n, err := reducer("css", w, r)
 	if err != nil {
 		return err

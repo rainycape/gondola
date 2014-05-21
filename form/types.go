8badf487b1ac1a242d439285600eef79349f0944
Alberto García Hierro <alberto@garciahierro.com>
2014-05-21 10:48:42 +0100

    Add HasChoices method to Type

--- form/types.go
+++ form/types.go
@@ -18,3 +18,10 @@ const (
 	// <select>
 	SELECT
 )
+
+// HasChoices returns wheter the type has multiple
+// choices, which corresponds to RADIO and SELECT
+// elements.
+func (t Type) HasChoices() bool {
+	return t == RADIO || t == SELECT
+}

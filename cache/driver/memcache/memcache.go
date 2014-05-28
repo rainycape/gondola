3f945dbe9f52dabb9a86f6e200275b1a8036c119
Alberto García Hierro <alberto@garciahierro.com>
2014-05-28 06:06:26 +0100

    Don't report memcache timeouts as errors
    They're so frequent in high load conditions that they generate
    a lot of noise.

--- cache/driver/memcache/memcache.go
+++ cache/driver/memcache/memcache.go
@@ -6,6 +6,7 @@ import (
 	"github.com/rainycape/memcache"
 	"gnd.la/cache/driver"
 	"gnd.la/config"
+	"net"
 	"strings"
 	"time"
 )
@@ -16,13 +17,13 @@ type memcacheDriver struct {
 
 func (c *memcacheDriver) Set(key string, b []byte, timeout int) error {
 	item := memcache.Item{Key: key, Value: b, Expiration: int32(timeout)}
-	return c.Client.Set(&item)
+	return c.error(c.Client.Set(&item))
 }
 
 func (c *memcacheDriver) Get(key string) ([]byte, error) {
 	item, err := c.Client.Get(key)
-	if err != nil && err != memcache.ErrCacheMiss {
-		return nil, err
+	if err != nil {
+		return nil, c.error(err)
 	}
 	if item != nil {
 		return item.Value, nil
@@ -32,8 +33,8 @@ func (c *memcacheDriver) Get(key string) ([]byte, error) {
 
 func (c *memcacheDriver) GetMulti(keys []string) (map[string][]byte, error) {
 	results, err := c.Client.GetMulti(keys)
-	if err != nil && err != memcache.ErrCacheMiss {
-		return nil, err
+	if err != nil {
+		return nil, c.error(err)
 	}
 	value := make(map[string][]byte, len(results))
 	for k, v := range results {
@@ -43,15 +44,26 @@ func (c *memcacheDriver) GetMulti(keys []string) (map[string][]byte, error) {
 }
 
 func (c *memcacheDriver) Delete(key string) error {
-	err := c.Client.Delete(key)
-	if err != nil && err != memcache.ErrCacheMiss {
-		return err
-	}
-	return nil
+	return c.error(c.Client.Delete(key))
 }
 
 func (c *memcacheDriver) Connection() interface{} {
 	return c.Client
+}
+
+func (c *memcacheDriver) error(err error) error {
+	if err != nil {
+		if err == memcache.ErrCacheMiss {
+			return nil
+		}
+		if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
+			// Don't log these errors since they're so frequent
+			// in memcache that they end up generating a lot
+			// of logs
+			return nil
+		}
+	}
+	return err
 }
 
 func memcacheOpener(url *config.URL) (driver.Driver, error) {

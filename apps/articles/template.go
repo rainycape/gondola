c3cd27be9e539261d4380625df9c1b606e248d5c
Alberto García Hierro <alberto@garciahierro.com>
2014-05-28 21:57:17 +0100

    When reversing an article, search in parent and included apps

--- apps/articles/template.go
+++ apps/articles/template.go
@@ -11,6 +11,12 @@ func reverseArticle(ctx *app.Context, article interface{}) (string, error) {
 }
 
 func reverseAppArticle(a *app.App, article interface{}) (string, error) {
+	checked := make(map[*app.App]bool)
+	return reverseAppsArticle(a, article, checked)
+}
+
+func reverseAppsArticle(a *app.App, article interface{}, checked map[*app.App]bool) (string, error) {
+	checked[a] = true
 	articles := AppArticles(a)
 	switch x := article.(type) {
 	case string:
@@ -19,11 +25,24 @@ func reverseAppArticle(a *app.App, article interface{}) (string, error) {
 				return a.Reverse(ArticleHandlerName, v.Slug())
 			}
 		}
-		return "", fmt.Errorf("no article with id %q found", x)
 	case *Article:
 		return a.Reverse(ArticleHandlerName, x.Slug())
 	case Article:
 		return a.Reverse(ArticleHandlerName, x.Slug())
+	}
+	if p := a.Parent(); p != nil && !checked[p] {
+		return reverseAppsArticle(p, article, checked)
+	}
+	for _, v := range a.Included() {
+		if checked[v] {
+			continue
+		}
+		if url, err := reverseAppsArticle(v, article, checked); err == nil {
+			return url, nil
+		}
+	}
+	if id, ok := article.(string); ok {
+		return "", fmt.Errorf("no article with id %q found", id)
 	}
 	return "", fmt.Errorf("can't reverse Article from %T, must be *Article or string (article id)", article)
 }

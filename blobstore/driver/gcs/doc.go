0c944fe03274ba292ea66a93126df060486a061c
Alberto García Hierro <alberto@garciahierro.com>
2014-07-30 15:21:45 +0200

    Document how to create the default GCS bucket in GAE
    Just because I've just spent half an hour looking for it, so I
    guess users will have the same problem.

--- blobstore/driver/gcs/doc.go
+++ blobstore/driver/gcs/doc.go
@@ -13,4 +13,10 @@
 //
 // Is the bucket is omitted, the default (your-app-id.appspot.com) is used.
 // The bucket must already exist.
+//
+// To create the default bucket from a GAE project without billing enabled, open
+// https://appengine.google.com, select your project, then go to Application
+// Settings in the left sidebar and scroll to the bottom. There's a section
+// labeled "Cloud Integration" just at the end with a "Create" button. Click
+// on it and the default GCS bucket will be created.
 package gcs

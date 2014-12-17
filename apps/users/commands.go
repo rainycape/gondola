5fd604470149ec74c21ee5ed15090e43e5f479b6
Alberto García Hierro <alberto@garciahierro.com>
2014-12-17 00:17:25 +0000

    Add commands for managing users to the users app

---
+++ apps/users/commands.go
@@ -0,0 +1,85 @@
+package users
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"gnd.la/app"
+	"gnd.la/commands"
+	"gnd.la/crypto/password"
+
+	"github.com/bgentry/speakeasy"
+)
+
+func registerUser(ctx *app.Context) {
+	checkUserType(userType)
+	username := ctx.RequireIndexValue(0)
+	userVal, _ := newEmptyUser()
+	updating := false
+	if ctx.Orm().MustOne(ByUsername(username), userVal.Interface()) {
+		// Updating existing user
+		updating = true
+	} else {
+		// Creating a new one
+		userVal = newUser(username)
+	}
+	var askPassword bool
+	ctx.ParseParamValue("p", &askPassword)
+	if !updating || askPassword {
+		password1, err := speakeasy.Ask("Password:")
+		if err != nil {
+			panic(err)
+		}
+		password2, err := speakeasy.Ask("Confirm Password:")
+		if err != nil {
+			panic(err)
+		}
+		if password1 != password2 {
+			panic(fmt.Errorf("passwords don't match"))
+		}
+		setUserValue(userVal, "Password", password.New(password1))
+	}
+	var admin bool
+	ctx.ParseParamValue("s", &admin)
+	setUserValue(userVal, "Admin", admin)
+
+	var email string
+	ctx.ParseParamValue("e", &email)
+	if email != "" {
+		setUserValue(userVal, "Email", email)
+	}
+
+	ctx.Orm().MustSave(userVal.Interface())
+	ctx.Logger().Infof("saved user as %+v", userVal.Interface())
+}
+
+func listUsers(ctx *app.Context) {
+	checkUserType(userType)
+	userVal, ptr := newEmptyUser()
+	iter := ctx.Orm().All().Iter()
+	w := tabwriter.NewWriter(os.Stdout, 0, 8, 2, '\t', tabwriter.Debug)
+	fmt.Fprint(w, "ID\tUsername\tEmail\tAdmin?\n")
+	for iter.Next(ptr) {
+		val := userVal.Elem().FieldByName("User").Interface().(User)
+		fmt.Fprintf(w, "%d\t%s\t%s\t%v\n", val.UserId, val.Username, val.Email, val.Admin)
+	}
+	if err := w.Flush(); err != nil {
+		panic(err)
+	}
+}
+
+func init() {
+	commands.Register(registerUser, &commands.Options{
+		Usage: "[-s | -p | -e email ] <username>",
+		Help:  "Registers a new user",
+		Flags: commands.Flags(
+			commands.BoolFlag("s", false, "Create an admin - if the user already exists is made an admin"),
+			commands.BoolFlag("p", false, "Update the user password, only used when updating a user"),
+			commands.StringFlag("e", "", "Email for the created user"),
+		),
+	})
+	commands.Register(listUsers, &commands.Options{
+		Help: "List all registered users",
+	})
+}
